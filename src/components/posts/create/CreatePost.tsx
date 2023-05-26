1e0977b4ecb1396892c3c40740b133dcd153c9e9
ValeryVerkhoturov <verkhoturov.valery@icloud.com>
2023-05-26 23:11:25 +0300

    fix: create navigation

--- src/components/posts/create/CreatePost.tsx
+++ src/components/posts/create/CreatePost.tsx
@@ -148,7 +148,10 @@ export default function CreatePost(props: CreatePostProps) {
               Отменить
             </button>
             <button
-              onClick={() => createPost.mutate({ ...post, reason: props.postItemReason })}
+              onClick={(e) => {
+                e.preventDefault()
+                createPost.mutate({ ...post, reason: props.postItemReason })
+              }}
               className='ml-3 inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
             >
               Сохранить
