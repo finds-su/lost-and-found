af5d728c0b637b6932222394506d5c0d317074c6
ValeryVerkhoturov <verkhoturov.valery@icloud.com>
2023-05-28 12:35:15 +0300

    fix: constants

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,3 +1,3 @@
 export const minNicknameLength = 5
 export const telegramUsernameRegex = /(?=.{5,64}(?:\s|$))(?![_])(?!.*[_]{2})[a-zA-Z0-9_]+(?<![_.])/
-export const aIRateLimitPerDay = 100
+export const aIRateLimitPerDay = 3
