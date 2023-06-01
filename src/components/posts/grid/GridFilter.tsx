7a3104fd782413415596b5e97912cecf59f90b2b
ValeryVerkhoturov <verkhoturov.valery@icloud.com>
2023-06-01 18:35:59 +0300

    fix: className typo

--- src/components/posts/grid/GridFilter.tsx
+++ src/components/posts/grid/GridFilter.tsx
@@ -159,7 +159,7 @@ export default function GridFilter(props: GridFilterProps) {
                                 }
                               }}
                               type='checkbox'
-                              className='biorder-gray-300 h-4 w-4 rounded text-blue-600 focus:ring-blue-500'
+                              className='h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500'
                             />
                             <label
                               htmlFor={`filter-${section.id}-${optionIdx}`}
