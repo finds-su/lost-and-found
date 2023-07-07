4d0491b167bb74badc0dd34753577151127f5482
ValeryVerkhoturov <verkhoturov.valery@icloud.com>
2023-07-07 18:44:15 +0300

    refactor: decompose scroll grid component

--- src/components/posts/grid/infinite-scroll-grid-with-filter/infinite-scroll-grid-with-filter.tsx
+++ src/components/posts/grid/infinite-scroll-grid-with-filter/infinite-scroll-grid-with-filter.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import Image from 'next/image'
 import { type PostItemReason } from '@prisma/client'
@@ -9,12 +9,36 @@ import GridFilter from '@/components/posts/grid/grid-filter'
 import useScrollGridStore from '@/lib/hooks/store/scroll-grids-store'
 import Spinner from '@/components/spinner'
 
-export default function InfiniteScrollGridWithFilter(props: { reason: PostItemReason }) {
-  const { enabledSortOption, checkedFilters } = useScrollGridStore((state) => state[props.reason])
+function ScrollGridLoader() {
+  return (
+    <p className='col-span-2 flex justify-center py-5 text-center md:col-span-4'>
+      <Spinner />
+      <span className='sr-only'>Загрузка...</span>
+    </p>
+  )
+}
+
+function ScrollGridEndMessage() {
+  return (
+    <div className='col-span-2 flex flex-col items-center text-sm text-gray-600 md:col-span-4'>
+      <Image src='/assets/illustrations/box.png' alt='' width={200} height={200} priority={false} />
+      Объявлений больше не найдено
+    </div>
+  )
+}
+
+interface InfiniteScrollGridWithFilterProps {
+  reason: PostItemReason
+}
+
+export default function InfiniteScrollGridWithFilter({
+  reason,
+}: InfiniteScrollGridWithFilterProps) {
+  const { enabledSortOption, checkedFilters } = useScrollGridStore((state) => state[reason])
   const postsQuery = api.posts.infinitePosts.useInfiniteQuery(
     {
       limit: 12,
-      reason: props.reason,
+      reason,
       orderByCreationDate: enabledSortOption,
       filters: checkedFilters,
     },
@@ -39,38 +63,22 @@ export default function InfiniteScrollGridWithFilter(props: { reason: PostItemRe
   }
 
   return (
-    <div>
-      <GridFilter reason={props.reason} />
+    <>
+      <GridFilter reason={reason} />
       <InfiniteScroll
         dataLength={posts.length}
         next={fetchMoreData}
         hasMore={hasMore}
-        loader={
-          <p className='col-span-2 flex justify-center py-5 text-center md:col-span-4'>
-            <Spinner />
-            <span className='sr-only'>Загрузка...</span>
-          </p>
-        }
-        endMessage={
-          <div className='col-span-2 flex flex-col items-center text-sm text-gray-600 md:col-span-4'>
-            <Image
-              src='/assets/illustrations/box.png'
-              alt=''
-              width={200}
-              height={200}
-              priority={false}
-            />
-            Объявлений больше не найдено
-          </div>
-        }
+        loader={<ScrollGridLoader />}
+        endMessage={<ScrollGridEndMessage />}
         className='grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-1 lg:gap-x-8'
       >
         {posts.map((post) => (
           <div key={post.id} className='group relative'>
             <div className='h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80'>
               <Image
-                src={post.images[0] ? post.images[0] : '/assets/placeholder.svg'}
-                alt={''}
+                src={post.images[0] ?? '/assets/placeholder.svg'}
+                alt=''
                 width={800}
                 height={800}
                 className='h-full w-full object-cover object-center'
@@ -79,7 +87,7 @@ export default function InfiniteScrollGridWithFilter(props: { reason: PostItemRe
               />
             </div>
             <h3 className='mt-4 overflow-hidden text-ellipsis text-sm text-gray-900'>
-              <a href={`${props.reason === 'LOST' ? 'losses' : 'finds'}/${post.id}`}>
+              <a href={`${reason === 'LOST' ? 'losses' : 'finds'}/${post.id}`}>
                 <span className='absolute inset-0' />
                 {post.name}
               </a>
@@ -90,6 +98,6 @@ export default function InfiniteScrollGridWithFilter(props: { reason: PostItemRe
           </div>
         ))}
       </InfiniteScroll>
-    </div>
+    </>
   )
 }
