abda642f8d0320f3a8039a7964d07372f6261c2d
ValeryVerkhoturov <verkhoturov.valery@icloud.com>
2023-07-11 17:30:36 +0300

    fix: date formatting

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,14 +1,27 @@
+const months: { [name: number]: string } = {
+  1: 'янв.',
+  2: 'фев.',
+  3: 'марта',
+  4: 'апр.',
+  5: 'мая',
+  6: 'июня',
+  7: 'июля',
+  8: 'авг.',
+  9: 'сент.',
+  10: 'окт.',
+  11: 'нояб.',
+  12: 'дек.',
+}
+
 export function formatDate(rawDate: string) {
   const date = new Date(rawDate)
   const day = date.getDate()
-  const month = date.getMonth() + 1
+  const month = months[date.getMonth() + 1]
   const year = date.getFullYear()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-  return `${Math.floor(day / 10) != 0 ? day : '0' + day.toString()}.${
-    Math.floor(month / 10) != 0 ? month : '0' + month.toString()
-  }.${year} ${Math.floor(hour / 10) != 0 ? hour : '0' + hour.toString()}:${
-    Math.floor(minute / 10) != 0 ? minute : '0' + minute.toString()
-  }:${Math.floor(second / 10) != 0 ? second : '0' + second.toString()}`
+
+  const nowYear = new Date().getFullYear()
+  // const hour = date.getHours()
+  // const minute = date.getMinutes()
+  // const second = date.getSeconds()
+  return `${day} ${month ?? ''} ${year === nowYear ? '' : year}`
 }
